ddd0a3ae42c3e10c789f7bad80e80c6a14a73a60
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-06 16:14:01 -0500

    One method for startgame.

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,13 @@
 public class Game {
 
     public static void main(String[] args) {
+        System.out.println("Looking Good");
 
 
+    }
+
+    public static void startGame() {
+        System.out.println("")
     }
 
 }
