9b84749786e80eb503cbc3a53a79298fabb151d9
Ronnel M Palencia <ronnel.m.palencia@gmail.com>
2020-05-07 09:30:38 -0500

    Added more choice in each room.

--- src/Game.java
+++ src/Game.java
@@ -21,10 +21,13 @@ public class Game {
     }
 
     public static void pickRoom(Scanner input) {
+        byte choice = 0;
         byte room1 = 1;
         byte room2 = 2;
         byte room3 = 3;
-        byte health = 0;
+        byte potion = 3;
+        boolean key = false;
+        byte health = 5;
 
         while (health != 0) {
             System.out.println("Pick a room: ");
@@ -34,12 +37,42 @@ public class Game {
                 pickRoom(input);
             } else if (pickRoom == room2) {
                 System.out.println("You are in room 2");
+                System.out.println("This is the kitchen. Do you want to make a potion?");
+                System.out.println("1: Make potion");
+                System.out.println("2: Leave");
+                choice = input.nextByte();
+                if(choice == 1) {
+                    System.out.println("Making Potion!");
+                    health++;
+                    System.out.println("Your health is: " + health);
+                    pickRoom(input);
+                } else {
+                    System.out.println("You are leaving");
+                    System.out.println("You are back in the main lobby.");
+                    pickRoom(input);
+                }
                 pickRoom(input);
             } else if (pickRoom == room3) {
                 System.out.println("You are in room 3");
+                System.out.println("There is a big spider in the room. What do you want to do?");
+                System.out.println("1: Fight");
+                System.out.println("2: Run");
+                choice = input.nextByte();
+                if(choice == 1) {
+                    System.out.println("Fight!");
+                } else {
+                    System.out.println("You are running!");
+                    System.out.println("You are back in the main lobby.");
+                    pickRoom(input);
+                }
                 pickRoom(input);
             } else {
-                System.out.println("You found yo friend!");
+                if(key == true) {
+                    System.out.println("Open door.");
+                    System.out.println("You found yo friend!");
+                } else {
+                    System.out.println("Door is closed.");
+                }
             }
         }
         System.out.println("You Died!");
