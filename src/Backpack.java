feb509806eb34180965d380039b2e69ef0dd9b79
Valeria Meza <valeriameza173@gmail.com>
2020-05-09 22:53:02 -0500

    working on the backpack feature. Where the user can find itmes and add it to their backpack.

---
+++ src/Backpack.java
@@ -0,0 +1,15 @@
+public class Backpack {
+    private String[] backpack = {};
+
+    public String[] getBackpack() {
+        return backpack;
+    }
+
+    public void setBackpack(String[] backpack) {
+        this.backpack = backpack;
+    }
+
+    public String[] addToBackPack(String[] add) {
+        return add;
+    }
+}
