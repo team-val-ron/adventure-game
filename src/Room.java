1dd11b7933a2a3b8b83aaeeb18b18885221af3c8
Valeria Meza <valeriameza173@gmail.com>
2020-05-09 22:43:00 -0500

    added room locations and added some fun ascii art when you find your friend.

--- src/Room.java
+++ src/Room.java
@@ -19,9 +19,39 @@ public class Room {
 
         while(true) {
             if (user >= min && user <= max) {
-                System.out.println("You are now in room: " + user);
+                System.out.println();
             } else {
                 System.out.println("Hmmm try another room...");
+                System.out.println();
+            }
+            if(user == 1) {
+
+                System.out.println("You are in the living room.");
+                System.out.println("---------------------------");
+                System.out.println();
+            } else if(user == 2) {
+                System.out.println("You are in the kitchen.");
+                System.out.println("-----------------------");
+                System.out.println();
+            } else if(user == 3) {
+                System.out.println("You are in the bathroom");
+                System.out.println("-----------------------");
+                System.out.println();
+            } else if(user == 4) {
+                System.out.println("You are in the garage");
+                System.out.println("---------------------");
+                System.out.println();
+            } else if(user == 5) {
+                System.out.println("You found your friend!");
+                System.out.println("----------------------");
+                System.out.println("\n" +
+                        "____    ____  ______    __    __     ____    __    ____  __  .__   __.  __  \n" +
+                        "\\   \\  /   / /  __  \\  |  |  |  |    \\   \\  /  \\  /   / |  | |  \\ |  | |  | \n" +
+                        " \\   \\/   / |  |  |  | |  |  |  |     \\   \\/    \\/   /  |  | |   \\|  | |  | \n" +
+                        "  \\_    _/  |  |  |  | |  |  |  |      \\            /   |  | |  . `  | |  | \n" +
+                        "    |  |    |  `--'  | |  `--'  |       \\    /\\    /    |  | |  |\\   | |__| \n" +
+                        "    |__|     \\______/   \\______/         \\__/  \\__/     |__| |__| \\__| (__) \n" +
+                        "                                                                            \n");
             }
             return enterRoom(min, max);
         }
