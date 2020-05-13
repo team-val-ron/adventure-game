a83e5837a8f2c3491e928f86f136d38d47a1208b
Valeria Meza <valeriameza173@gmail.com>
2020-05-12 22:53:07 -0500

    working on the backpack feat.

--- src/Backpack.java
+++ src/Backpack.java
@@ -1,6 +1,7 @@
 public class Backpack {
     public static addToBackpack[] searchItems() {
         return new addToBackpack[] {
+                new addToBackpack("Throw some Hands", true),
                 new addToBackpack("Bug Spray", false),
                 new addToBackpack("Nerf Gun", false),
                 new addToBackpack("Sandal", false),
--- src/addToBackpack.java
+++ src/addToBackpack.java
@@ -1,10 +1,19 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class addToBackpack {
+    Scanner input = new Scanner(System.in);
     private String item;
     private boolean foundItem = false;
 
     public addToBackpack(String item, boolean found) {
         this.item = item;
         this.foundItem = found;
+        System.out.println("Select weapon: ");
+        input.nextLine();
+        if(this.item.equalsIgnoreCase("Throw some Hands")) {
+            System.out.println("You chose: Throw some Hands.");
+        }
     }
 
     public String getItem() {
